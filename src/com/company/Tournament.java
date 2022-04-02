8a66aefdc762cb0f98b8f79ab973aa8686c98b2e
PelleErobreren <pelledahl22@gmail.com>
2022-04-02 14:30:00 +0200

    Ændringer i JFrame

--- src/com/company/Tournament.java
+++ src/com/company/Tournament.java
@@ -41,13 +41,33 @@ public class Tournament
         button2 = new JButton();
         JButton button3;
         button3 = new JButton();
-        button1.setText("Se holdscore");
-        button2.setText("Opret et nyt hold");
-        button3.setText("Start ny turnering");
+        JButton button4;
+        button4 = new JButton();
+        JButton button5;
+        button5 = new JButton();
+        JButton button6;
+        button6 = new JButton();
+        JButton button7;
+        button7 = new JButton();
+        JButton button8;
+        button8 = new JButton();
+        button1.setText("Opret turnering");
+        button2.setText("Tag imod tilmeldinger");
+        button3.setText("Planlæg kampe");
+        button4.setText("Registrer kampresultater");
+        button5.setText("Tilføj hold til de indledende kampe");
+        button6.setText("Tilføj hold til semifinalerne");
+        button7.setText("Tilføj hold til finalekampen");
+        button8.setText("Alle tilmeldte hold");
         panel.add(label);
         panel.add(button1);
         panel.add(button2);
         panel.add(button3);
+        panel.add(button4);
+        panel.add(button5);
+        panel.add(button6);
+        panel.add(button7);
+        panel.add(button8);
         frame.add(panel);
         frame.setSize(300, 400);
         frame.setLocationRelativeTo(null);
@@ -59,11 +79,11 @@ public class Tournament
         // TODO ProgressMonitor-klassen kan også sætte en dialog op, der viser forløbet af en operation,
         // TODO men den giver udfordringer på input siden
 
-        button1.addActionListener(e ->
+        button8.addActionListener(e ->
         {
             try
             {
-                button1Pressed();
+                button8Pressed();
             }
             catch (IOException ex)
             {
@@ -84,13 +104,12 @@ public class Tournament
         });
     }
 
-    public static void button1Pressed() throws IOException
+    public static void button8Pressed() throws IOException
     {
         String[] score;
         score = new String[]{"Hold 1", "Hold 2", "Hold 3", "Hold 4", "Hold 5", "Hold 6", "Hold 7", "Hold 8"};
         int result;
-        result = JOptionPane.showOptionDialog(null,
-                "Se holdscore på:", "Holdscore",
+        result = JOptionPane.showOptionDialog(null, "Tryk for at se holdets spillere + score:", "Holdscore",
                 JOptionPane.OK_CANCEL_OPTION,
                 JOptionPane.QUESTION_MESSAGE,
                 null, score, score[0]);
