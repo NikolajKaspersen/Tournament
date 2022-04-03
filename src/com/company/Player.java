c839582de052a58401bd663d0543e6dc33ba3352
Chriswihu <99390466+Chriswihu@users.noreply.github.com>
2022-04-03 17:05:31 +0200

    Commiting Player class

---
+++ src/com/company/Player.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Player
+{
+    private String name;
+
+
+    public Player(String name) {
+
+    }
+
+    public String getPlayerName()
+    {
+        return name;
+    }
+
+    public void setPlayerName(String playerName)
+    {
+    }
+}
--- src/com/company/Registration.java
+++ src/com/company/Registration.java
@@ -4,8 +4,20 @@ public class Registration {
     String nameOfTeam;
     String [] nameOfPlayers;
 
-    public void recieveRegistraions() {
+    private String name;
 
+
+    public Registration(){
+        this.name = name;
+
+    }
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
 
     }
 }
