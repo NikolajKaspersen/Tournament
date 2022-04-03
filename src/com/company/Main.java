2da6902e8c3e93146039a3015a537d0b4272abe8
PelleErobreren <pelledahl22@gmail.com>
2022-04-03 19:50:56 +0200

    Konsol koden

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,15 +11,13 @@ public class Main {
 // TODO knap 3 skal registre match result
 // TODO knap 4 skal lave et nyt Tournament
 // TODO knap 5 skal vise result for være kamp
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException
+    {
         //TournamentSystem.runTournament();
-
         //System.out.println();
         //TournamentSystem.runTournament();
         //Tournament tournament = new Tournament();
-        Tournament.tournamentFrame();
         Tournament.createTournament();
-
     }
 }
